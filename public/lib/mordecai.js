8d4e7b2a9fdacb3641cf5278731fe6ccc0880da5
Quinn C. Hoyer <quinn@themightyquinn.com>
2017-02-18 10:16:51 -0900

    Hole selection redone

--- public/lib/mordecai.js
+++ public/lib/mordecai.js
@@ -1,38 +1,18 @@
-let gameId;
-const guesses = [];
+"use strict";
+
 const answerPegs = 4;
 let currentGuess = new Array(4);
 let selectedHole;
-let currentRow;
-
-function handleHoleClick(event) {
-  selectedHole = event.data.hole;
-
-  $(event.currentTarget).addClass('selected');
-}
-
-function addRow() {
-  const board = $('.board').append('<div class="row"></div>');
-  
-  currentRow = board.children('.row').prepend($('<div class="key-peg-hole"></div>').css('visibility', 'hidden'));
-  
-  for (let i = 0; i < answerPegs; i += 1) {
-    let hole = $(`<div class="code-peg-hole"><div class="code-peg"></div></div>`);
-    hole.click({ hole: i }, handleHoleClick);
-    currentRow.append(hole);
-  }
-
-  currentGuess = Array(answerPegs);
-}
 
 function setGuessHole(color) {
   currentGuess[selectedHole] = color;
 
-  const row = $('.board .row:first-child');
-  row.forEach((item, index) => {
+  const row = $('.guess-current .row');
+  row.children('div.code-peg-hole').each((index, item) => {
+    item = $(item);
     item.removeClass(() => {
       let classes = [];
-      for (let i = 0; i < answerPegs; i += 1) { 
+      for (let i = 0; i < answerPegs; i += 1) {
         classes[i] = `p-${i}`;
       }
 
@@ -42,6 +22,32 @@ function setGuessHole(color) {
     item.addClass(`p-${color}`);
   });
 }
+
+function addGuessCurrentRow() {
+  const row = $('<div class="row flex"></div>');
+
+  for (let i = 0; i <= answerPegs; i += 1) { 
+    let hole = $('<div class="code-peg-hole"><div class="code-peg"></div></div>');
+    hole.click({ hole: i }, function (evt) {
+      row.children().removeClass('selected');
+      row.children().eq([evt.data.hole]).addClass('selected');
+    });
+    row.append(hole);
+  }
+
+  let submit = $('<div class="submit-pane"></div>').css('visibility', 'hidden');
+  submit.click(function (evt) {
+    //TODO: Process click
+  });
+
+  row.append(submit);
+
+  $('.guess-current').append(row);
+}
+
+function addGuessHistoryRow() {
+}
+
 
 function listenOnCodePegs() {
   $('.code-pegs')
@@ -55,12 +61,6 @@ function listenOnCodePegs() {
 }
 
 $(function () {
-
-  addRow();
-
-
-
-  $('.start-button').click(function () {
-
-  });
+  addGuessCurrentRow();
+  // listenOnCodePegs();
 });
