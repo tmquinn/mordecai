5fe60767628a7c18ce7a6de48e88f909b16155c7
Quinn C. Hoyer <quinn@themightyquinn.com>
2017-02-18 08:30:16 -0900

    Getting ready to do a little refactor
    Going to make a static guess row and make some helper classes for flex

--- public/lib/mordecai.js
+++ public/lib/mordecai.js
@@ -1,26 +1,64 @@
 let gameId;
 const guesses = [];
 const answerPegs = 4;
+let currentGuess = new Array(4);
+let selectedHole;
+let currentRow;
 
-function handleHoleClick (event) {
-  console.log(event.data.hole);
+function handleHoleClick(event) {
+  selectedHole = event.data.hole;
+
+  $(event.currentTarget).addClass('selected');
 }
 
 function addRow() {
   const board = $('.board').append('<div class="row"></div>');
-  const keyPegHole = $('<div class="key-peg-hole"></div>').css('visibility', 'hidden');
-  const row = board.children('.row').append(keyPegHole);
-  for (let i = 0; i < answerPegs; i += 1) { 
+  
+  currentRow = board.children('.row').prepend($('<div class="key-peg-hole"></div>').css('visibility', 'hidden'));
+  
+  for (let i = 0; i < answerPegs; i += 1) {
     let hole = $(`<div class="code-peg-hole"><div class="code-peg"></div></div>`);
-    hole.click({hole: i}, handleHoleClick);
-    row.append(hole);
+    hole.click({ hole: i }, handleHoleClick);
+    currentRow.append(hole);
   }
 
+  currentGuess = Array(answerPegs);
+}
+
+function setGuessHole(color) {
+  currentGuess[selectedHole] = color;
+
+  const row = $('.board .row:first-child');
+  row.forEach((item, index) => {
+    item.removeClass(() => {
+      let classes = [];
+      for (let i = 0; i < answerPegs; i += 1) { 
+        classes[i] = `p-${i}`;
+      }
+
+      return classes.join(' ');
+    });
+
+    item.addClass(`p-${color}`);
+  });
+}
+
+function listenOnCodePegs() {
+  $('.code-pegs')
+    .children('div.code-peg-hole')
+    .forEach(function (hole, index) {
+      hole.click({ hole: index }, (evt) => {
+        //TODO: Check that I am not already selected
+        setGuessHole(evt.data.hole);
+      });
+    });
 }
 
 $(function () {
 
   addRow();
+
+
 
   $('.start-button').click(function () {
 
