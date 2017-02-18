e4d9392511eeb8a17c6a0e3328b631f9e178dc61
Quinn C. Hoyer <quinn@themightyquinn.com>
2017-02-18 10:44:02 -0900

    Select peg functinoality

--- public/lib/mordecai.js
+++ public/lib/mordecai.js
@@ -1,36 +1,39 @@
 "use strict";
 
 const answerPegs = 4;
+const numColors = 6;
 let currentGuess = new Array(4);
-let selectedHole;
+let selectedHole = -1;
 
 function setGuessHole(color) {
-  currentGuess[selectedHole] = color;
+  if (selectedHole !== -1) {
+    currentGuess[selectedHole] = color;
 
-  const row = $('.guess-current .row');
-  row.children('div.code-peg-hole').each((index, item) => {
-    item = $(item);
-    item.removeClass(() => {
-      let classes = [];
-      for (let i = 0; i < answerPegs; i += 1) {
+    const currentPeg = $(`.guess-current .row`).children().eq(selectedHole).children('.code-peg');
+
+    currentPeg.removeClass(() => {
+      const classes = [];
+      for (let i = 0; i < numColors; i += 1) {
         classes[i] = `p-${i}`;
       }
-
       return classes.join(' ');
     });
 
-    item.addClass(`p-${color}`);
-  });
+    currentPeg.addClass(`p-${color}`);
+  }
 }
 
 function addGuessCurrentRow() {
   const row = $('<div class="row flex"></div>');
 
-  for (let i = 0; i <= answerPegs; i += 1) { 
+  for (let i = 0; i < answerPegs; i += 1) {
     let hole = $('<div class="code-peg-hole"><div class="code-peg"></div></div>');
+
+    //Guess Current Click Handler
     hole.click({ hole: i }, function (evt) {
       row.children().removeClass('selected');
       row.children().eq([evt.data.hole]).addClass('selected');
+      selectedHole = evt.data.hole;
     });
     row.append(hole);
   }
@@ -48,12 +51,11 @@ function addGuessCurrentRow() {
 function addGuessHistoryRow() {
 }
 
-
 function listenOnCodePegs() {
   $('.code-pegs')
     .children('div.code-peg-hole')
-    .forEach(function (hole, index) {
-      hole.click({ hole: index }, (evt) => {
+    .each(function (index, hole) {
+      $(hole).click({ hole: index }, (evt) => {
         //TODO: Check that I am not already selected
         setGuessHole(evt.data.hole);
       });
@@ -62,5 +64,5 @@ function listenOnCodePegs() {
 
 $(function () {
   addGuessCurrentRow();
-  // listenOnCodePegs();
+  listenOnCodePegs();
 });
